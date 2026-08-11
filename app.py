0bea79030784c6bc390fbd27617935393e51342f
KaranNegi08 <karannegi.agra@gmail.com>
2026-08-11 15:04:56 +0530

    updated app file

--- app.py
+++ app.py
@@ -32,18 +32,8 @@ tender_service = TenderService()
 available_tenders = tender_service.list_available_tenders()
 
 # Initialize Standardized Session State Variables
-if "active_tender_id" not in st.session_state:
-    st.session_state.active_tender_id = available_tenders[0] if available_tenders else "GEM_9146015"
-if "indexed_files" not in st.session_state:
-    st.session_state.indexed_files = []
-if "vendor_dossiers" not in st.session_state:
-    st.session_state.vendor_dossiers = []
-if "comparison_result" not in st.session_state:
-    st.session_state.comparison_result = None
-if "export_files" not in st.session_state:
-    st.session_state.export_files = None
-if "tender_indexed" not in st.session_state:
-    st.session_state.tender_indexed = False
+from utils.session_state import init_session_state
+init_session_state(available_tenders)
 
 logger.info("Main landing page loaded.")
 
